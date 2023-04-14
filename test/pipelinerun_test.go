1122cba61187896267d23f98e8064fe862ef7168
Emma Munley <emmamunley@google.com>
2023-04-14 16:10:43 -0400

    test

--- test/pipelinerun_test.go
+++ test/pipelinerun_test.go
@@ -744,7 +744,7 @@ metadata:
 spec:
   params:
   - name: HELLO
-    value: "Hello World!" 
+    value: "Hello World!"
   pipelineRef:
     name: %s
 `, namespace, pipelineName))
@@ -951,4 +951,182 @@ func getLimitRange(name, namespace, resourceCPU, resourceMemory, resourceEphemer
 			},
 		},
 	}
+}
+
+func TestMatrix(t *testing.T) {
+	t.Parallel()
+	type tests struct {
+		name                   string
+		testSetup              func(ctx context.Context, t *testing.T, c *clients, namespace string, index int) *v1beta1.Pipeline
+		expectedTaskRuns       []string
+		expectedNumberOfEvents int
+		pipelineRunFunc        func(*testing.T, int, string, string) *v1beta1.PipelineRun
+	}
+
+	tds := []tests{{
+		name: "fan-in and fan-out",
+		testSetup: func(ctx context.Context, t *testing.T, c *clients, namespace string, _ int) *v1beta1.Pipeline {
+			t.Helper()
+			tasks := getFanInFanOutTasksMatrix(t, namespace)
+			for _, task := range tasks {
+				if _, err := c.V1beta1TaskClient.Create(ctx, task, metav1.CreateOptions{}); err != nil {
+					t.Fatalf("Failed to create Task `%s`: %s", task.Name, err)
+				}
+			}
+
+			p := getFanInFanOutPipelineMatrix(t, namespace, tasks)
+			if _, err := c.V1beta1PipelineClient.Create(ctx, p, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create Pipeline `%s`: %s", p.Name, err)
+			}
+
+			return p
+		},
+		pipelineRunFunc:  getFanInFanOutPipelineRunMatrix,
+		expectedTaskRuns: []string{"platforms-and-browsers"},
+		// 1 from PipelineRun and 4 from Tasks defined in pipelinerun
+		expectedNumberOfEvents: 9,
+	}}
+
+	var requiredAlphaFeatureFlags = requireAnyGate(map[string]string{
+		"enable-api-fields": "alpha",
+	})
+
+	for i, td := range tds {
+		i := i   // capture range variable
+		td := td // capture range variable
+		t.Run(td.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+
+			c, namespace := setup(ctx, t, requiredAlphaFeatureFlags)
+			knativetest.CleanupOnInterrupt(func() { tearDown(ctx, t, c, namespace) }, t.Logf)
+			defer tearDown(ctx, t, c, namespace)
+
+			t.Logf("Setting up test resources for %q test in namespace %s", td.name, namespace)
+			p := td.testSetup(ctx, t, c, namespace, i)
+
+			pipelineRun := td.pipelineRunFunc(t, i, namespace, p.Name)
+			prName := pipelineRun.Name
+			_, err := c.V1beta1PipelineRunClient.Create(ctx, pipelineRun, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("Failed to create PipelineRun `%s`: %s", prName, err)
+			}
+
+			t.Logf("Waiting for PipelineRun %s in namespace %s to complete", prName, namespace)
+			if err := WaitForPipelineRunState(ctx, c, prName, timeout, PipelineRunSucceed(prName), "PipelineRunSuccess", v1beta1Version); err != nil {
+				t.Fatalf("Error waiting for PipelineRun %s to finish: %s", prName, err)
+			}
+			t.Logf("Making sure the expected TaskRuns %s were created", td.expectedTaskRuns)
+			actualTaskrunList, err := c.V1beta1TaskRunClient.List(ctx, metav1.ListOptions{LabelSelector: fmt.Sprintf("tekton.dev/pipelineRun=%s", prName)})
+			if err != nil {
+				t.Fatalf("Error listing TaskRuns for PipelineRun %s: %s", prName, err)
+			}
+			expectedTaskRunNames := []string{}
+			for _, runName := range td.expectedTaskRuns {
+				taskRunName := strings.Join([]string{prName, runName}, "-")
+				// check the actual task name starting with prName+runName with a random suffix
+				for _, actualTaskRunItem := range actualTaskrunList.Items {
+					if strings.HasPrefix(actualTaskRunItem.Name, taskRunName) {
+						taskRunName = actualTaskRunItem.Name
+					}
+				}
+				expectedTaskRunNames = append(expectedTaskRunNames, taskRunName)
+				r, err := c.V1beta1TaskRunClient.Get(ctx, taskRunName, metav1.GetOptions{})
+				if err != nil {
+					t.Fatalf("Couldn't get expected TaskRun %s: %s", taskRunName, err)
+				}
+				if !r.Status.GetCondition(apis.ConditionSucceeded).IsTrue() {
+					t.Fatalf("Expected TaskRun %s to have succeeded but Status is %v", taskRunName, r.Status)
+				}
+
+				t.Logf("Checking that labels were propagated correctly for TaskRun %s", r.Name)
+				checkLabelPropagation(ctx, t, c, namespace, prName, r)
+				t.Logf("Checking that annotations were propagated correctly for TaskRun %s", r.Name)
+				checkAnnotationPropagation(ctx, t, c, namespace, prName, r)
+			}
+
+			matchKinds := map[string][]string{"PipelineRun": {prName}, "TaskRun": expectedTaskRunNames}
+
+			t.Logf("Making sure %d events were created from taskrun and pipelinerun with kinds %v", td.expectedNumberOfEvents, matchKinds)
+
+			events, err := collectMatchingEvents(ctx, c.KubeClient, namespace, matchKinds, "Succeeded")
+			if err != nil {
+				t.Fatalf("Failed to collect matching events: %q", err)
+			}
+			if len(events) != td.expectedNumberOfEvents {
+				collectedEvents := ""
+				for i, event := range events {
+					collectedEvents += fmt.Sprintf("%#v", event)
+					if i < (len(events) - 1) {
+						collectedEvents += ", "
+					}
+				}
+				t.Fatalf("Expected %d number of successful events from pipelinerun and taskrun but got %d; list of receieved events : %#v", td.expectedNumberOfEvents, len(events), collectedEvents)
+			}
+
+			t.Logf("Successfully finished test %q", td.name)
+		})
+	}
+}
+
+func getFanInFanOutTasksMatrix(t *testing.T, namespace string) map[string]*v1beta1.Task {
+	t.Helper()
+	return map[string]*v1beta1.Task{
+		"platforms-and-browsers": parse.MustParseV1beta1Task(t, fmt.Sprintf(`
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  params:
+    - name: platform
+      default: ""
+    - name: browser
+      default: ""
+  steps:
+    - name: echo
+      image: alpine
+      script: |
+        echo "$(params.platform) and $(params.browser)"
+`, helpers.ObjectNameForTest(t), namespace)),
+	}
+}
+
+func getFanInFanOutPipelineMatrix(t *testing.T, namespace string, tasks map[string]*v1beta1.Task) *v1beta1.Pipeline {
+	t.Helper()
+	return parse.MustParseV1beta1Pipeline(t, fmt.Sprintf(`
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  tasks:
+  - name: platforms-and-browsers
+    taskRef:
+      name: %s
+    matrix:
+      params:
+        - name: platform
+          value:
+            - linux
+            - mac
+            - windows
+        - name: browser
+          value:
+            - chrome
+            - safari
+            - firefox
+`, helpers.ObjectNameForTest(t), namespace, tasks["platforms-and-browsers"].Name))
+}
+
+func getFanInFanOutPipelineRunMatrix(t *testing.T, _ int, namespace string, pipelineName string) *v1beta1.PipelineRun {
+	t.Helper()
+	return parse.MustParseV1beta1PipelineRun(t, fmt.Sprintf(`
+metadata:
+  name: %s
+  namespace: %s
+spec:
+  pipelineRef:
+    name: %s
+`, helpers.ObjectNameForTest(t), namespace, pipelineName))
 }
