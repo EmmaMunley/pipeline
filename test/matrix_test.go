82476d0e7bbcc7e6beb5ac4a3e2ac8ccdf244fdb
Emma Munley <emmamunley@google.com>
2023-04-17 17:04:25 -0400

    passing e2e test in table format

--- test/matrix_test.go
+++ test/matrix_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"github.com/tektoncd/pipeline/test/diff"
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"github.com/tektoncd/pipeline/test/parse"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/pkg/apis"
@@ -50,18 +51,24 @@ var (
 // TestPipelineRunMatrixed is an integration test that will
 // verify that pipelinerun works with a matrixed pipelinerun
 func TestPipelineRunMatrixed(t *testing.T) {
+
 	t.Parallel()
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	fmt.Println("TEST")
-	c, namespace := setup(ctx, t, requireAnyGate(alphaFeatureFlags))
-	knativetest.CleanupOnInterrupt(func() { tearDown(context.Background(), t, c, namespace) }, t.Logf)
-	defer tearDown(context.Background(), t, c, namespace)
-	t.Logf("Creating Task in namespace %s", namespace)
-	task := parse.MustParseV1beta1Task(t, fmt.Sprintf(`
+	type tests struct {
+		name                     string
+		task                     *v1beta1.Task
+		getTask                  func(namespace string) *v1beta1.Task
+		getPipeline              func(namespace string, taskName string) *v1beta1.Pipeline
+		getPipelineRun           func(namespace string, pipelineName string) *v1beta1.PipelineRun
+		expectedTaskRunNames     []string
+		expectedNumberOfTaskRuns int
+	}
+
+	tds := []tests{{
+		name: "matrix",
+		getTask: func(namespace string) *v1beta1.Task {
+			return parse.MustParseV1beta1Task(t, fmt.Sprintf(`
 metadata:
-  name: mytask
+  name: platforms-and-browsers
   namespace: %s
 spec:
   params:
@@ -77,10 +84,9 @@ spec:
       script: |
         echo "$(params.platform) and $(params.browser)"
 `, namespace))
-	if _, err := c.V1beta1TaskClient.Create(ctx, task, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create Task `%s`: %s", task.Name, err)
-	}
-	pipeline := parse.MustParseV1beta1Pipeline(t, fmt.Sprintf(`
+		},
+		getPipeline: func(namespace string, taskName string) *v1beta1.Pipeline {
+			return parse.MustParseV1beta1Pipeline(t, fmt.Sprintf(`
 metadata:
   name: %s
   namespace: %s
@@ -106,225 +112,85 @@ spec:
           value: v0.33.0
       taskRef:
         name:  %s
-`, helpers.ObjectNameForTest(t), namespace, task.Name))
-	pipelineRun := parse.MustParseV1beta1PipelineRun(t, fmt.Sprintf(`
+`, helpers.ObjectNameForTest(t), namespace, taskName))
+		},
+		getPipelineRun: func(namespace string, pipelineName string) *v1beta1.PipelineRun {
+			return parse.MustParseV1beta1PipelineRun(t, fmt.Sprintf(`
 metadata:
   name: %s
   namespace: %s
 spec:
-  serviceAccountName: "default"
   pipelineRef:
     name: %s
-`, helpers.ObjectNameForTest(t), namespace, pipeline.Name))
-	if _, err := c.V1beta1PipelineClient.Create(ctx, pipeline, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create Pipeline `%s`: %s", pipeline.Name, err)
-	}
-	if _, err := c.V1beta1PipelineRunClient.Create(ctx, pipelineRun, metav1.CreateOptions{}); err != nil {
-		t.Fatalf("Failed to create PipelineRun `%s`: %s", pipelineRun.Name, err)
-	}
-	name := "matrix"
-	prName := pipelineRun.Name
-	expectedTaskRunNames := []string{"platforms-and-browsers-0", "platforms-and-browsers-1", "platforms-and-browsers-2", "platforms-and-browsers-3", "platforms-and-browsers-4", "platforms-and-browsers-5", "platforms-and-browsers-6", "platforms-and-browsers-7", "platforms-and-browsers-8"}
-	expectedTaskRuns := []*v1beta1.TaskRun{
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-0", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: chrome
-  - name: platform
-    value: linux
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-1", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: chrome
-  - name: platform
-    value: mac
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-2", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: chrome
-  - name: platform
-    value: windows
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-3", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: safari
-  - name: platform
-    value: linux
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-4", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: safari
-  - name: platform
-    value: mac
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-5", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: safari
-  - name: platform
-    value: windows
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-6", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: firefox
-  - name: platform
-    value: linux
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-7", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: firefox
-  - name: platform
-    value: mac
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-		mustParseTaskRunWithObjectMeta(t,
-			taskRunObjectMeta("pr-platforms-and-browsers-8", namespace,
-				prName, pipeline.Name, task.Name, false),
-			`
-spec:
-  params:
-  - name: browser
-    value: firefox
-  - name: platform
-    value: windows
-  - name: version
-    value: v0.33.0
-  serviceAccountName: default
-  taskRef:
-    name: mytask
-    kind: Task
-`),
-	}
-	expectedNumberOfEvents := 1
-	t.Logf("Waiting for PipelineRun %s in namespace %s to complete", prName, namespace)
-	if err := WaitForPipelineRunState(ctx, c, prName, timeout, PipelineRunSucceed(prName), "PipelineRunSuccess", v1beta1Version); err != nil {
-		t.Fatalf("Error waiting for PipelineRun %s to finish: %s", prName, err)
-	}
-	t.Logf("Making sure the expected TaskRuns %s were created", expectedTaskRunNames)
-	actualTaskrunList, err := c.V1beta1TaskRunClient.List(ctx, metav1.ListOptions{LabelSelector: fmt.Sprintf("tekton.dev/pipelineRun=%s", prName)})
-	if err != nil {
-		t.Fatalf("Error listing TaskRuns for PipelineRun %s: %s", prName, err)
-	}
+`, helpers.ObjectNameForTest(t), namespace, pipelineName))
+		},
+		expectedTaskRunNames:     []string{"platforms-and-browsers-0", "platforms-and-browsers-1", "platforms-and-browsers-2", "platforms-and-browsers-3", "platforms-and-browsers-4", "platforms-and-browsers-5", "platforms-and-browsers-6", "platforms-and-browsers-7", "platforms-and-browsers-8"},
+		expectedNumberOfTaskRuns: 9,
+	}}
 
-	for i := range actualTaskrunList.Items {
-		expectedTaskRun := expectedTaskRuns[i]
-		fmt.Println("expectedTaskRun", expectedTaskRun.Spec)
-		fmt.Println("actual", actualTaskrunList.Items[i].Spec)
-		// expectedTaskRun.Labels["tekton.dev/pipeline"] = tt.name
-		// expectedTaskRun.Labels["tekton.dev/memberOf"] = tt.memberOf
-		if d := cmp.Diff(expectedTaskRun.Spec, actualTaskrunList.Items[i].Spec, ignoreResourceVersion, ignoreTypeMeta); d != "" {
-			t.Errorf("expected to see TaskRun %v created. Diff %s", expectedTaskRuns[i].Name, diff.PrintWantGot(d))
-		}
-	}
+	for _, td := range tds {
+		td := td // capture range variable
+		t.Run(td.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
 
-	for _, taskRunName := range expectedTaskRunNames {
-		for _, actualTaskRunItem := range actualTaskrunList.Items {
-			if strings.HasSuffix(actualTaskRunItem.Name, taskRunName) {
-				taskRunName = actualTaskRunItem.Name
+			c, namespace := setup(ctx, t, requireAnyGate(alphaFeatureFlags))
+			fmt.Println("namespace", namespace)
+			knativetest.CleanupOnInterrupt(func() { tearDown(context.Background(), t, c, namespace) }, t.Logf)
+			defer tearDown(context.Background(), t, c, namespace)
+			t.Logf("Creating Task in namespace %s", namespace)
+			task := td.getTask(namespace)
+			if _, err := c.V1beta1TaskClient.Create(ctx, task, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create Task `%s`: %s", task.Name, err)
 			}
-		}
+			pipeline := td.getPipeline(namespace, task.Name)
+			if _, err := c.V1beta1PipelineClient.Create(ctx, pipeline, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create Pipeline `%s`: %s", pipeline.Name, err)
+			}
+			pipelineRun := td.getPipelineRun(namespace, pipeline.Name)
+			if _, err := c.V1beta1PipelineRunClient.Create(ctx, pipelineRun, metav1.CreateOptions{}); err != nil {
+				t.Fatalf("Failed to create PipelineRun `%s`: %s", pipelineRun.Name, err)
+			}
+			prName := pipelineRun.Name
+			t.Logf("Waiting for PipelineRun %s in namespace %s to complete", prName, namespace)
+			if err := WaitForPipelineRunState(ctx, c, prName, timeout, PipelineRunSucceed(prName), "PipelineRunSuccess", v1beta1Version); err != nil {
+				t.Fatalf("Error waiting for PipelineRun %s to finish: %s", prName, err)
+			}
+			t.Logf("Making sure the expected TaskRuns %s were created", td.expectedTaskRunNames)
+			actualTaskrunList, err := c.V1beta1TaskRunClient.List(ctx, metav1.ListOptions{LabelSelector: fmt.Sprintf("tekton.dev/pipelineRun=%s", prName)})
+			if err != nil {
+				t.Fatalf("Error listing TaskRuns for PipelineRun %s: %s", prName, err)
+			}
 
-		r, err := c.V1beta1TaskRunClient.Get(ctx, taskRunName, metav1.GetOptions{})
-		if err != nil {
-			t.Fatalf("Couldn't get expected TaskRun %s: %s", taskRunName, err)
-		}
-		if !r.Status.GetCondition(apis.ConditionSucceeded).IsTrue() {
-			t.Fatalf("Expected TaskRun %s to have succeeded but Status is %v", taskRunName, r.Status)
-		}
+			if len(actualTaskrunList.Items) != (td.expectedNumberOfTaskRuns) {
+				t.Fatalf("Expected `%d` of task runs to be created, but got %d", td.expectedNumberOfTaskRuns, len(actualTaskrunList.Items))
+			}
 
-		if _, err := c.V1beta1PipelineRunClient.Get(ctx, pipelineRun.Name, metav1.GetOptions{}); err != nil {
-			t.Fatalf("Failed to get PipelineRun `%s`: %s", pipelineRun.Name, err)
-		}
+			for _, taskRunName := range td.expectedTaskRunNames {
+				for _, actualTaskRunItem := range actualTaskrunList.Items {
+					if strings.HasSuffix(actualTaskRunItem.Name, taskRunName) {
+						taskRunName = actualTaskRunItem.Name
+					}
+				}
 
-		t.Logf("Checking that labels were propagated correctly for TaskRun %s", r.Name)
-		checkLabelPropagation(ctx, t, c, namespace, prName, r)
-		t.Logf("Checking that annotations were propagated correctly for TaskRun %s", r.Name)
-		checkAnnotationPropagation(ctx, t, c, namespace, prName, r)
+				r, err := c.V1beta1TaskRunClient.Get(ctx, taskRunName, metav1.GetOptions{})
+				if err != nil {
+					t.Fatalf("Couldn't get expected TaskRun %s: %s", taskRunName, err)
+				}
+				if !r.Status.GetCondition(apis.ConditionSucceeded).IsTrue() {
+					t.Fatalf("Expected TaskRun %s to have succeeded but Status is %v", taskRunName, r.Status)
+				}
+
+				if _, err := c.V1beta1PipelineRunClient.Get(ctx, pipelineRun.Name, metav1.GetOptions{}); err != nil {
+					t.Fatalf("Failed to get PipelineRun `%s`: %s", pipelineRun.Name, err)
+				}
+
+				t.Logf("Checking that labels were propagated correctly for TaskRun %s", r.Name)
+				checkLabelPropagation(ctx, t, c, namespace, prName, r)
+				t.Logf("Checking that annotations were propagated correctly for TaskRun %s", r.Name)
+				checkAnnotationPropagation(ctx, t, c, namespace, prName, r)
 
 		matchKinds := map[string][]string{"PipelineRun": {prName}, "TaskRun": expectedTaskRunNames}
 
@@ -342,11 +208,23 @@ spec:
 					collectedEvents += ", "
 				}
 			}
-			t.Fatalf("Expected %d number of successful events from pipelinerun and taskrun but got %d; list of receieved events : %#v", expectedNumberOfEvents, len(events), collectedEvents)
 		}
-
-		t.Logf("Successfully finished test %q", name)
 	}
+	return om
+}
+
+func mustParseTaskRunWithObjectMeta(t *testing.T, objectMeta metav1.ObjectMeta, asYAML string) *v1beta1.TaskRun {
+	t.Helper()
+	tr := parse.MustParseV1beta1TaskRun(t, asYAML)
+	tr.ObjectMeta = objectMeta
+	return tr
+}
+
+func mustParseCustomRunWithObjectMeta(t *testing.T, objectMeta metav1.ObjectMeta, asYAML string) *v1beta1.CustomRun {
+	t.Helper()
+	r := parse.MustParseCustomRun(t, asYAML)
+	r.ObjectMeta = objectMeta
+	return r
 }
 
 func taskRunObjectMeta(trName, ns, prName, pipelineName, pipelineTaskName string, skipMemberOfLabel bool) metav1.ObjectMeta {
